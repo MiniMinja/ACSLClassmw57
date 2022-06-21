38b78f16643a23a1d20faff4e3eed9cb25ec8f06
MiniMinja <minyoungheo.heo@gmail.com>
2022-06-20 18:01:50 -0700

    finished 2021 junior contest 1 acsl

---
+++ ACSLPrograms/src/_2021jrc1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class _2021jrc1 {
+	static Scanner in;
+	
+	static int s, d, r;
+	
+	static int sum;
+	
+	public static void main(String[] args) {
+		init();
+		solve();
+		output();
+	}
+	
+	public static void init() {
+		in = new Scanner(System.in);
+		s = in.nextInt();
+		d = in.nextInt();
+		r = in.nextInt();
+		sum = 0;
+	}
+	
+	public static void solve() {
+		
+	}
+	
+	public static void output() {
+		
+	}
+}
